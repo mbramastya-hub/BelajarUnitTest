c27f2c61c3169d215a3323a3093f14402d52c057
abc <abc@example.com>
2020-03-10 17:50:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/belajarunittest/SharedPreferenceEntry.java
@@ -0,0 +1,31 @@
+package com.example.belajarunittest;
+
+import java.util.Calendar;
+
+public class SharedPreferenceEntry {
+    /**
+     * Model class containing personal information that will be saved to SharedPreferences.
+     */
+
+        // Name of the user.
+        private final String mName;
+        // Date of Birth of the user.
+        private final Calendar mDateOfBirth;
+        // Email address of the user.
+        private final String mEmail;
+        public SharedPreferenceEntry(String name, Calendar dateOfBirth, String email) {
+            mName = name;
+            mDateOfBirth = dateOfBirth;
+            mEmail = email;
+        }
+        public String getName() {
+            return mName;
+        }
+        public Calendar getDateOfBirth() {
+            return mDateOfBirth;
+        }
+        public String getEmail() {
+            return mEmail;
+        }
+    }
+
